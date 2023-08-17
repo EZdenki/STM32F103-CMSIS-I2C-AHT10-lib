118f1d416d1be86d49ab92b873eee16fc3151fae
Mike <sandynomike@gmail.com>
2023-08-17 22:03:18 +0900

    Updated comments

--- STM32F103-CMSIS-I2C-lib.c
+++ STM32F103-CMSIS-I2C-lib.c
@@ -5,8 +5,6 @@
 // Target Microcontroller: STM32F103 (Blue Pill)
 // Mike Shegedin, 05/2023   Started
 //
-// Requires the STM32F103-Pause-lib.c library.
-//
 // Code to implement the following routines.
 // Note that the desired target I2C interface, I2C1 or I2C2, will be passed to *thisI2C.
 // The interface name is passed as-is, like "I2C_init( I2C1 );".
